a7762e17e0f2b19e4d87ab42304839b9960a1720
bzeng <bzeng@ucvts.tec.nj.us>
2017-12-28 06:15:58 +0000

    got one step to work and added the first scenario

--- final-project/game1.py
+++ final-project/game1.py
@@ -21,24 +21,35 @@ class Character(object):
             'food': 0,
             'clothes': 2,
             'money': 50}
-            self.status = {
-                'day': 1,
-                'health': 100,
-                'miles to next checkpoint': 50}
+            self.day = 1
+            self.distance_left = 25
+            self.total_distance = 100
+            self.health = 100
             print(f"""Welcome, {name}. You wake up with only the clothes on your back and $50 in your pocket.""")
         def list_inventory(self):
             for k, v in self.inventory.items():
-                print(f"{k} --- {v}")
+                print(f"""{k} --- {v}""")
         def list_status(self):
-            for k,v in self.status.items():
-                print(f"{k} --- {v}")
+            print(f"""
+            Day --- {self.day}
+            Distance until next checkpoint --- {self.distance_left}
+            Health --- {self.health}
+            """)
         def list_walk(self, miles = 5):
-            for value in self.status.values():
-                self.status.value('miles to next checkpoint') -= miles
+            self.distance_left -= miles
+            self.total_distance -= miles
+            self.day += 1
+            if self.inventory['food'] > 0:
+                self.inventory['food'] -= 2
+            print(f"""
+
+            Day: {self.day} --- Next Checkpoint in: {self.distance_left} miles
+
+            """)
+
 
 #defining the character
-a = Character(input('What is your name?\n - '))
-
+a = Character(input("""What is your name? \n - """))
 
 # #a robber class
 # class Robber(object):
@@ -46,18 +57,44 @@ a = Character(input('What is your name?\n - '))
 #         self.stolen =
 
 #actions
-print(input("""What would you like to do?
+b = int(input(f"""What would you like to do?
 1. Inventory
 2. Status
-3. Go forward\n - """))
-if 1:
+3. Go forward (miles left: {a.distance_left})\n - """))
+
+
+if b == 1:
     a.list_inventory()
-elif 2:
+elif b == 2:
     a.list_status()
-elif 3:
+elif b == 3:
     a.list_walk()
 else:
-    print('invalid input. try again')
+    print("""invalid input. try again \n - """)
+
+
+if a.total_distance == 95:
+    c = int(input("""You approach the edge of the dense forest.
+On the outskirts of the trees seems to be a sketchy hut with an old man sitting in a lawn chair outside it.
+What do you do?
+1. Inventory
+2. Status
+3. Approach the sketchy vendor
+4. Keep on walking - you don't want to cause any trouble \n - """))
+    if c == 1:
+        a.list_inventory()
+    elif c == 2:
+        a.list_status()
+    elif c == 3:
+        int(input("""You slowly walk towards the vendor, and he greets you with a smile.
+        'Would you like to buy something?' he says. 'Here are my prices:'
+            1. food --- $1 per pound
+            2. clothes --- $3 per piece
+            3. life advice --- $5\n - """))
+    elif c == 4:
+        a.list_walk()
+    else:
+        print("""invalid input. try again \n - """)
 
 
 # a.inventory{'food'} += 1
