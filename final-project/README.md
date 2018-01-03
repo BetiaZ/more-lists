5a884d10b20b59142062cf6d8d6362fd298b18df
bzeng <bzeng@ucvts.tec.nj.us>
2018-01-03 15:42:04 +0000

    made readme file and made a simple loop of checkpoints

---
+++ final-project/README.md
@@ -0,0 +1,12 @@
+The game is a survival game, where the player needs to make it to the other side of an island in the shortest amount of time possible.
+The player has a character class, with an inventory, status (including day, health and distance left), and also the attribute to walk
+and cover distance.
+Each time, the player will be asked to choose from several options, including viewing their inventory, status, and moving forward.
+The total distance until the end of the game is 50 miles, and each checkpoint is every 10 miles.
+The player can move 5 miles at a time, and at each checkpoint the player has more options of what to do. For example, they can hunt,
+buy supplies, or rest.
+Uses while loops so that the player can choose more than one option each time they are presented with them.
+
+One thing that I would like to do is, everytime the player walks, there is a possiblity that something (bad or good) will happen to them,
+For example, they might lose all their clothes, fall in a river, or find food.
+It depends if I have time to code this part.
--- final-project/game1.py
+++ final-project/game1.py
@@ -3,6 +3,10 @@
 # - can hunt (random pounds of food), set how many miles you want to go per day
 # - goal: get to the end in the shortest number of days possible
 # - random factors: lose clothes in river, bad weather delays, food spoils, find food or abandoned supplies
+#checkpoint 2: nothing, can build a shelter? possibly
+#c3: store again
+#c4: river- if you choose the wrong option, (i.e., you realize that you don't know how to swim) you DIE
+#c5: you're alive! if you have enough money left, you can take the boat home, if you don't, you realize you will have to survive on the island for eternity
 
 #introduction
 print("""ISLAND ADVENTURE
@@ -15,30 +19,37 @@ GOOD LUCK (YOU'RE GONNA NEED IT).""")
 #creating a character
 class Character(object):
         """Character class"""
+        # initializing the character
         def __init__(self, name):
             self.name = name
+            #storing the inventory in a dictionary
             self.inventory = {
             'food': 0,
             'clothes': 2,
             'money': 50}
+            #starting statuses
             self.day = 1
-            self.distance_left = 25
-            self.total_distance = 100
+            self.distance_left = 10
+            self.total_distance = 50
             self.health = 100
             print(f"""Welcome, {name}. You wake up with only the clothes on your back and $50 in your pocket.""")
+        # if this function is called, the inventory will be printed
         def list_inventory(self):
             for k, v in self.inventory.items():
                 print(f"""{k} --- {v}""")
+        # if this function is called, the status will be printed
         def list_status(self):
             print(f"""
             Day --- {self.day}
             Distance until next checkpoint --- {self.distance_left}
             Health --- {self.health}
             """)
+        # the character walks forward, and different scenario occurs at each distance
         def list_walk(self, miles = 5):
             self.distance_left -= miles
             self.total_distance -= miles
             self.day += 1
+            #food decreases with each day
             if self.inventory['food'] > 0:
                 self.inventory['food'] -= 2
             print(f"""
@@ -51,29 +62,29 @@ class Character(object):
 #defining the character
 a = Character(input("""What is your name? \n - """))
 
-# #a robber class
-# class Robber(object):
-#     def __init__(self, name):
-#         self.stolen =
-
 #actions
 b = int(input(f"""What would you like to do?
 1. Inventory
 2. Status
 3. Go forward (miles left: {a.distance_left})\n - """))
 
+#using if and elif statements to have different options and results
+while a.total_distance == 50:
+    if b == 1:
+        a.list_inventory()
+        break
+    elif b == 2:
+        a.list_status()
+        break
+    elif b == 3:
+        a.list_walk()
+    else:
+        print("""invalid input. try again \n - """)
 
-if b == 1:
-    a.list_inventory()
-elif b == 2:
-    a.list_status()
-elif b == 3:
-    a.list_walk()
-else:
-    print("""invalid input. try again \n - """)
 
 
-if a.total_distance == 95:
+#first scenario occurs after walking the first time, or when the total distance left is 95
+while a.total_distance == 45:
     c = int(input("""You approach the edge of the dense forest.
 On the outskirts of the trees seems to be a sketchy hut with an old man sitting in a lawn chair outside it.
 What do you do?
@@ -86,15 +97,31 @@ What do you do?
     elif c == 2:
         a.list_status()
     elif c == 3:
-        int(input("""You slowly walk towards the vendor, and he greets you with a smile.
+        d = int(input("""You slowly walk towards the vendor, and he greets you with a smile.
         'Would you like to buy something?' he says. 'Here are my prices:'
             1. food --- $1 per pound
             2. clothes --- $3 per piece
             3. life advice --- $5\n - """))
+        if d == 1:
+            pounds = int(input("How many pounds? (will be rounded to the nearest whole #) \n - "))
+            cost_f = pounds * 1
+            self.inventory['food'] += pounds
+            self.inventory['money'] -= cost_f
+            print(f"""You bought {pounds} pounds of food. You now have {self.inventory['money']} dollars.""")
+        elif d == 2:
+            pieces = int(input("How many? (will be rounded to the nearest whole #) \n - "))
+            cost_c = pieces * 3
+            self.inventory['clothes'] += pieces
+            self.inventory['money'] -= cost_c
+            print(f"""You bought {pieces} pieces of clothing. You now have {self.inventory['money']} dollars.""")
     elif c == 4:
         a.list_walk()
+        break
     else:
         print("""invalid input. try again \n - """)
+
+#if a.distanceleft == 40:
+#if a.distanceleft == 30:
 
 
 # a.inventory{'food'} += 1
